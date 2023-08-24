4576fd6056f530c722c3932244ea5dddff6e8324
emanoel <emanoelalvesjr@gmail.com>
2023-08-24 20:13:51 -0300

    fix write in file

--- scripts/gyroAccPub.py
+++ scripts/gyroAccPub.py
@@ -47,30 +47,30 @@ if __name__ == '__main__':
     rate = rospy.Rate(3)
     gyroPub = rospy.Publisher('/gyroscope_publisher', Point, queue_size=60)
     accPub = rospy.Publisher('/accelerometer_publisher', Point, queue_size=120)
-    while not rospy.is_shutdown():
-        acc_x = read_raw_data(ACCEL_XOUT_H)
-        acc_y = read_raw_data(ACCEL_YOUT_H)
-        acc_z = read_raw_data(ACCEL_ZOUT_H)	
-        gyro_x = read_raw_data(GYRO_XOUT_H)
-        gyro_y = read_raw_data(GYRO_YOUT_H)
-        gyro_z = read_raw_data(GYRO_ZOUT_H)	
-        Ax = (acc_x/16384.0-0.101778)*9.81 #16384 -> 1g 
-        Ay = (acc_y/16384.0+0.007739)*9.81
-        Az = (acc_z/16384.0-0.987720)*9.81	
-        Gx = gyro_x/131.0
-        Gy = gyro_y/131.0
-        Gz = gyro_z/131.0
-        Acc = Point(Ax, Ay, Az)
-        Acc = Point(Ax, 0, 0)
-        Gyro = Point(Gx, Gy, Gz)
-        Gyro = Point(0, 0, Gz)
-        A = "Ax: {}  Ay: {}  Az: {}\n".format(str(Ax),str(Ay),str(Az))
-        G = "Ax: {}  Ay: {}  Az: {}\n".format(str(Ax),str(Ay),str(Az))
-        with open("gyro.txt","w") as f:
+    with open("gyro.txt","w") as f:
+        while not rospy.is_shutdown():
+                acc_x = read_raw_data(ACCEL_XOUT_H)
+                acc_y = read_raw_data(ACCEL_YOUT_H)
+                acc_z = read_raw_data(ACCEL_ZOUT_H)	
+                gyro_x = read_raw_data(GYRO_XOUT_H)
+                gyro_y = read_raw_data(GYRO_YOUT_H)
+                gyro_z = read_raw_data(GYRO_ZOUT_H)	
+                Ax = (acc_x/16384.0-0.101778)*9.81 #16384 -> 1g 
+                Ay = (acc_y/16384.0+0.007739)*9.81
+                Az = (acc_z/16384.0-0.987720)*9.81	
+                Gx = gyro_x/131.0
+                Gy = gyro_y/131.0
+                Gz = gyro_z/131.0
+                Acc = Point(Ax, Ay, Az)
+                Acc = Point(Ax, 0, 0)
+                Gyro = Point(Gx, Gy, Gz)
+                Gyro = Point(0, 0, Gz)
+                A = "Ax: {}  Ay: {}  Az: {}\n".format(str(Ax),str(Ay),str(Az))
+                G = "Ax: {}  Ay: {}  Az: {}\n".format(str(Ax),str(Ay),str(Az))
                 f.write(A)
                 f.write(G)
-        print A
-        print G
-        gyroPub.publish(Gyro)
-        accPub.publish(Acc)
-        rate.sleep()
+                print A
+                print G
+                gyroPub.publish(Gyro)
+                accPub.publish(Acc)
+                rate.sleep()
