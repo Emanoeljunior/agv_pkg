4df409427585ba8365b827da08ac79be724c5974
emanoel <emanoelalvesjr@gmail.com>
2023-10-24 21:06:30 -0300

    update magnetometer

--- scripts/magnetPub.py
+++ scripts/magnetPub.py
@@ -3,20 +3,18 @@ import smbus
 import time	
 import rospy
 import py_qmc5883l
-from std_msgs.msg import String
+from geometry_msgs.msg import Point
 
 sensor = py_qmc5883l.QMC5883L()
 
 def compass():
-    pub = rospy.Publisher('magnet', String, queue_size=10)
+    pub = rospy.Publisher('magnet', Point, queue_size=10)
     rospy.init_node('compass', anonymous=True)
     rate = rospy.Rate(60) # 60hz
     while not rospy.is_shutdown():
-        compass_str = "Compass deviation: %s" % sensor.get_magnet()
+        # compass_str = "Compass deviation: %s" % sensor.get_magnet()
         d = sensor.get_data()
-        compass_str = "Compass Data: [x: %s, y: %s, z: %s, t: %s]" % (d[0], d[1], d[2], d[3])
-        #rospy.loginfo(type(sensor.get_magnet()))
-        print compass_str
+        magnetometer = Point(d[0],d[1],d[2])
         pub.publish(compass_str)
         rate.sleep()
 
