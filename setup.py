3af754610211ab018d6b5e704d62625967210053
emanoel <emanoelalvesjr@gmail.com>
2023-10-28 19:28:24 -0300

    change setup

--- setup.py
+++ setup.py
@@ -4,13 +4,8 @@ from setuptools import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
-setup_qmc = generate_distutils_setup(
-    packages=['py_qmc5883l'],
+setup_args = generate_distutils_setup(
+    packages=['py_qmc5883l','py_mpu6050'],
     package_dir={'': 'src'})
 
-setup_mpu = generate_distutils_setup(
-    packages=['py_mpu6050'],
-    package_dir={'': 'src'})
-
-setup(**setup_qmc)
-setup(**setup_mpu)
+setup(**setup_args)
