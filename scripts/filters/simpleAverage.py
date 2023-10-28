1712c4b8baf1f19246b387b2948f0443797b30df
emanoel <emanoelalvesjr@gmail.com>
2023-10-28 20:27:51 -0300

    update

--- scripts/filters/simpleAverage.py
+++ scripts/filters/simpleAverage.py
@@ -8,17 +8,21 @@ from matplotlib import pyplot as plt
 class SimpleAverage :
     
     def __init__ (self, sub, pub):
-        self.k = 0
+        self.k = 1
         self.mean = Point(0,0,0)
         self.sub = rospy.Subscriber(sub, Point, self.simple_avarage, queue_size = 20)
         self.pub = rospy.Publisher(pub, Point, queue_size=20)
 
     def simple_avarage(self, data):
-        self.k += 1
+        # Just to simplify notation
         k = self.k
+        mean = self.mean
+        
         self.mean.x += ((k-1)/k)*mean.x + (1/k)*data.x
         self.mean.y += ((k-1)/k)*mean.y + (1/k)*data.y
         self.mean.z += ((k-1)/k)*mean.z + (1/k)*data.z
+        self.k += 1
+        
 
         self.pub.publish(self.mean)
     
