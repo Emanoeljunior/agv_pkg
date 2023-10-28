108d7a1c0cf8e82b31cfed54a878c0c8d3ae1f87
emanoel <emanoelalvesjr@gmail.com>
2023-10-28 16:46:34 -0300

    add units

--- scripts/calibration/calibrationGyroAccPub.py
+++ scripts/calibration/calibrationGyroAccPub.py
@@ -57,9 +57,9 @@ if __name__ == '__main__':
         Ax = (acc_x/16384.0)*9.8  # 9.8 = 1g -> Scale is set in 2g = 32768 and -2g = -32768 -> Ax[m/s²]
         Ay = (acc_y/16384.0)*9.8  # 9.8 = 1g -> Scale is set in 2g = 32768 and -2g = -32768 -> Ay[m/s²]    
         Az = (acc_z/16384.0)*9.8  # 9.8 = 1g -> Scale is set in 2g = 32768 and -2g = -32768 -> Az[m/s²]	
-        Gx = gyro_x/131.0
-        Gy = gyro_y/131.0
-        Gz = gyro_z/131.0
+        Gx = gyro_x/131.0 # -> Scale is set in 250°/s = 32768 and -250°/s = -32768 -> Gx[degres/s]
+        Gy = gyro_y/131.0 # -> Scale is set in 250°/s = 32768 and -250°/s = -32768 -> Gy[degres/s]
+        Gz = gyro_z/131.0 # -> Scale is set in 250°/s = 32768 and -250°/s = -32768 -> Gz[degres/s]
         Acc = Point(Ax, Ay, Az)
         Gyro = Point(Gx, Gy, Gz)
         gyroPub.publish(Gyro)
