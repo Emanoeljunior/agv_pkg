3250248343e943f51129e5e727632e5496f86385
emanoel <emanoelalvesjr@gmail.com>
2023-11-02 10:00:57 -0300

    test publish offset

--- scripts/calibration/calibrationMagnetPub.py
+++ scripts/calibration/calibrationMagnetPub.py
@@ -12,18 +12,43 @@ class Calibration():
         self.max = Point(0,0,0)
         self.min = Point(0,0,0)
         self.ofsset = Point(0,0,0)
+        
+        rospy.init_node('compass', anonymous=True)
+        
+        self.magnet_pub = rospy.Publisher('magnet', Point, queue_size=10)
+        self.magnet_bearing_pub = rospy.Publisher('magnet_bearning', Float64, queue_size=10)
+        
+        self.magnet_max_pub = rospy.Publisher('magnet_max', Point, queue_size=10)
+        self.magnet_min_pub = rospy.Publisher('magnet_min', Point, queue_size=10)
+        self.magnet_offset_pub = rospy.Publisher('magnet_offset', Point, queue_size=10)
+
 
     def get_offset(self, data):
         if data.x > self.max.x:
             self.max.x = data.x
-        print(dir(self.max))
-        print(dir(data))
-        print(self.max.x)
+        if data.y > self.max.y:
+            self.max.y = data.y
+        if data.z > self.max.z:
+            self.max.z = data.z
+            
+        if data.x < self.min.x:
+            self.min.x = data.x
+        if data.y < self.min.y:
+            self.min.y = data.y
+        if data.z < self.min.z:
+            self.min.z = data.z
+            
+        self.offset.x = (self.max.x + self.min.x)/2
+        self.offset.y = (self.max.y + self.min.y)/2
+        self.offset.z = (self.max.z + self.min.z)/2
+        
+        self.magnet_max_pub.publish(self.max)
+        self.magnet_min_pub.publish(self.min)
+        self.magnet_offset_pub.publish(self.offset)
+        
 
     def compass(self, sensor):
-        magnet_pub = rospy.Publisher('magnet', Point, queue_size=10)
-        magnet_bearing_pub = rospy.Publisher('magnet_bearning', Float64, queue_size=10)
-        rospy.init_node('compass', anonymous=True)
+       
         rate = rospy.Rate(10) # 10hz
         while not rospy.is_shutdown():
             d = sensor.get_data()
@@ -31,8 +56,8 @@ class Calibration():
             bearing = sensor.get_bearing_raw()
             magnetometer = Point(d[0],d[1],d[2])
             self.get_offset(magnetometer)
-            magnet_pub.publish(magnetometer)
-            magnet_bearing_pub.publish(bearing)
+            self.magnet_pub.publish(magnetometer)
+            self.magnet_bearing_pub.publish(bearing)
             rate.sleep()
     def end(self):
         print "Exited calibration magnet"
